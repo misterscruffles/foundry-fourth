4558c0610a5ad5295952f321063831d65dfee2bc
Diane Cochran <dianeofthemoon@gmail.com>
2020-11-18 21:33:04 -0800

    chore: updated gulp to watch TS and SASS

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,20 +1,33 @@
 import * as gulp from "gulp";
 import * as ts from "gulp-typescript";
 import * as sass from "gulp-sass";
+import * as sourcemaps from "gulp-sourcemaps";
 
 const tsConfig = ts.createProject("tsconfig.json");
+
 
 
 /**
  * Build Sass
  */
-function buildSass() {
+const compileTS = function () {
+    let tsResult = tsConfig.src().pipe(sourcemaps.init()).pipe(tsConfig()).pipe(sourcemaps.write()).pipe(gulp.dest('module'));
+    return tsResult;
+}
+
+const compileSass = function () {
     console.log("Rebuilding sass files");
-    return gulp.src("styles/*.sass").pipe(sass()).pipe(gulp.dest("css"));
+    return gulp.src("styles/**/*.sass").pipe(sass()).pipe(gulp.dest("css"));
 }
 
-function buildWatch() {    
-    gulp.watch("styles/**/*.sass", { ignoreInitial: false }, buildSass);
+const watchFiles = function () {
+    gulp.watch('styles/**/*.sass', compileSass);
+    gulp.watch('ts/**/*.ts', compileTS);
 }
 
-gulp.task("watch", buildWatch);
+
+gulp.task("watch", gulp.series(compileTS, compileSass, watchFiles));
+
+gulp.task("sass", compileSass);
+
+gulp.task("typescript", compileTS);
